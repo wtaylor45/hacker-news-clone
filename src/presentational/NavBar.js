b7b5ee07f58b2904828f43f529aa0a5d0779f661
Will Taylor <taylorw@udel.edu>
2019-02-18 19:44:27 -0500

    Added navbar

--- src/presentational/NavBar.js
+++ src/presentational/NavBar.js
@@ -2,8 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Nav = styled.nav`
-    /* display: flex;
-    flex-direction: row; */
+    display: flex;
+    flex-direction: row;
+    width: 100%;
 `
 
 const Li = styled.li`
@@ -18,10 +19,15 @@ const NavItem = styled.a`
     font-size: 15px;
 `
 
+const LastLi = styled(Li)`
+    margin-left: auto;
+    margin-right: 5px;
+`
+
 export const NavBar = ({pages}) => 
     <Nav>
         <Li>
-            <b><NavItem href="/">Faker news</NavItem></b>
+            <b><NavItem href="/">Faker News</NavItem></b>
         </Li>
         <Li>
             <NavItem href="/new">new</NavItem> |
@@ -29,4 +35,7 @@ export const NavBar = ({pages}) =>
         <Li>
             <NavItem href="/submit">submit</NavItem>
         </Li>
+        <LastLi>
+            <NavItem href="/login">login</NavItem>
+        </LastLi>
     </Nav>
